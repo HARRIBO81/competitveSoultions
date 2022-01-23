f1c95c04dd75cfef4fb66533719f65947d31f046
Ryan <ryanx8673@gmail.com>
2022-01-23 00:19:17 -0500

    two new problems :)

--- ccc09s4.cpp
+++ ccc09s4.cpp
@@ -11,35 +11,33 @@ vector<pii> adj[MM];
 vector<pii> q;
 priority_queue<pii, vector<pii>, greater<>> pq;
 
-void dijkstra(int source) {
-    dist[source] = 0, pq.emplace(source,0);
-    while(!pq.empty()) {
-        int curNode = pq.top().first; pq.pop();
-        for(pii i : adj[curNode]) {
-            int adjNode = i.first, weight = i.second;
-            if(dist[adjNode] > dist[curNode] + weight) {
-                dist[adjNode] = dist[curNode] + weight;
-                pq.push(make_pair(adjNode, dist[adjNode]));
-            }
-        }
-    }
-}
-
 int main() {
+    memset(dist, INT_MAX, sizeof dist);
     cin >> n >> t;
-    for(int i = 0, x, y, c; i < t; i++) {
+    for (int i = 0, x, y, c; i < t; i++) {
         cin >> x >> y >> c;
-        adj[x].emplace_back(y,c), adj[y].emplace_back(x,c);
+        adj[x].emplace_back(y, c), adj[y].emplace_back(x, c);
     }
     cin >> k;
-    for(int i = 0, z, pz; i < k; i++) {
+    for (int i = 0, z, pz; i < k; i++) {
         cin >> z >> pz;
         q.emplace_back(z, pz);
     }
     cin >> d;
-    for(pii i : q) {
-        memset(dist,INT_MAX,sizeof dist);
-        dijkstra(i.first);
-        cout << dist[d]+i.second << endl;
+    dist[d] = 0, pq.emplace(d, 0);
+    while (!pq.empty()) {
+        int curNode = pq.top().first;
+        pq.pop();
+        for (pii i : adj[curNode]) {
+            int adjNode = i.first, weight = i.second;
+            if (dist[adjNode] > dist[curNode] + weight) {
+                dist[adjNode] = dist[curNode] + weight;
+                pq.emplace(adjNode, dist[adjNode]);
+            }
+        }
     }
+    for (int i = 1; i <= n; i++) {
+        cout << dist[i] << endl;
+    }
+//    cout << ans;
 }
--- vmss15c1p4.cpp
+++ vmss15c1p4.cpp
@@ -4,7 +4,39 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+typedef pair<int, int> pii;
+const int MM = 2022; // ;)
+int t, n, m, g, cnt = 0;
+priority_queue<pii, vector<pii>, greater<>> pq;
+vector<pii> adj[MM];
+vector<int> foodBasic;
+vector<int> dist(MM,INT_MAX);
 
 int main() {
+    cin >> t >> n >> m >> g;
+    for(int i = 0, x; i < g; i++) {
+        cin >> x; foodBasic.push_back(x);
+    }
+    for(int i = 0, a, b, l; i < m; i++) {
+        cin >> a >> b >> l;
+        adj[a].emplace_back(b, l);
+    }
+    dist[0]=0, pq.emplace(0,0);
+    while(!pq.empty()) {
+        int curNode = pq.top().first; pq.pop();
+        for(pii i : adj[curNode]) {
+            int adjNode = i.first, weight = i.second;
+            if(dist[adjNode] > dist[curNode] + weight) {
+                dist[adjNode] = dist[curNode] + weight;
+                pq.emplace(adjNode, dist[adjNode]);
+            }
+        }
+    }
 
+    for(int i : foodBasic) {
+        if(dist[i] <= t) {
+            cnt++;
+        }
+    }
+    cout << cnt;
 }
